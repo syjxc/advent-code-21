10c53bedea59c3c05f0d2bcb6702317ba220a617
syjxc <95354854+syjxc@users.noreply.github.com>
2021-12-03 17:43:43 +0000

    Update day2 - part1.py

--- day2 - part1.py
+++ day2 - part1.py
@@ -8,16 +8,10 @@ with open('advent_input_2.txt') as file:
         for word in str(lines[i]).split():
             if word.isdigit() and "forward" in str(lines[i]):
                 horizontal.append(int(word))
-    
-print(sum(horizontal))
-print(horizontal)
-
-for i in range(len(lines)):
-        for word in str(lines[i]).split():
             if word.isdigit() and "up" in str(lines[i]):
                 depth.append(-int(word))
             if word.isdigit() and "down" in str(lines[i]):
                 depth.append(int(word))
     
+print(sum(horizontal))
 print(sum(depth))
-print(depth)
